1086e4a20e69eb2f94e3a5063841ded7f00f827b
devrick <lhpsystems0376@gmail.com>
2024-10-29 12:04:01 -0300

    App Comercio

---
+++ src/app/dashboard/components/header/index.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import Link from 'next/link'
+import styles from './styles.module.scss'
+import Image from 'next/image'
+//import logoImg from '/public/logo.svg'
+import logoImg from '/public/logo.png'
+import { LogOutIcon } from 'lucide-react'
+import { deleteCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+
+export function Header(){
+  const router = useRouter();
+
+  async function handleLogout(){
+    deleteCookie("session", { path: "/" } )
+    toast.success("Logout feito com sucesso!")
+    
+    router.replace("/")
+  }
+
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/dashboard">
+          <Image
+            alt="Logo LHPSYSTEMS Pizza"
+            src={logoImg}
+            width={170}
+            height={170}
+            priority={true}
+            quality={100}
+            className={styles.logo}
+          />
+        </Link>
+
+        <nav>
+          <Link href="/dashboard/category">
+            Categoria
+          </Link>
+          <Link href="/dashboard/product">
+            Produto
+          </Link>
+
+          <form action={handleLogout}>
+            <button type='submit'>
+              <LogOutIcon size={24} color="#FFF" />
+            </button>
+          </form>
+        </nav>
+      </div>
+    </header>
+  )
+}
